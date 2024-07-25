482d731fd9f8c34691f4ead7c7ffdd1fc41e21d0
Anderson <andersonecomp091@gmail.com>
2024-07-25 14:36:28 -0300

    Incluindo usecsv lib no projeto para realizar a importacao de dados

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <ImportOk />
+  <!-- <ImportOk /> -->
+  <usecsv-button importerKey="1e9a7b85-e645-4f6c-99c4-f97485071ba3">
+    Importar Data
+  </usecsv-button>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue';
-import ImportOk from './components/ImportOK.vue';
+// import ImportOk from './components/ImportOK.vue';
+import UseCSVButton from '@usecsv/vuejs3';
 
 export default {
   name: 'App',
   components: {
-    ImportOk
+    // ImportOk
+    "usecsv-button": UseCSVButton
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import UseCSVButton from '@usecsv/vuejs3'
 import 'bootstrap/dist/css/bootstrap.css';
 
-createApp(App).mount('#app')
+createApp(App).component("usecsv-button", UseCSVButton).mount('#app')
